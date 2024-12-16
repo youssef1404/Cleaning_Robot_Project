7b3791b71e2539f1da5ce4df8a14054fc931dbab
Youssef Moahemd <107321754+Youssefx10@users.noreply.github.com>
2024-12-16 19:23:38 +0200

    pid_controller.h

--- control/movement/pid_controller.h
+++ control/movement/pid_controller.h
@@ -3,27 +3,8 @@
 
 class PIDController {
 public:
-  PIDController(float Kp, float Ki, float Kd) {
-    _Kp = Kp;
-    _Ki = Ki;
-    _Kd = Kd;
-    _error = 0;
-    _integral = 0;
-    _lastError = 0;
-  }
-
-  float calculateOutput(float setpoint, float measurement) {
-    _error = setpoint - measurement;
-// Restrict the error
-  float maxError = 0; // to max error value
-    _error = constrain(_error, -maxError, maxError);
-    _integral += _error * dt;
-  float derivative = (_error - _lastError) / dt;
-    _lastError = _error;
-
-    float output = _Kp * _error + _Ki * _integral + _Kd * derivative;
-    return output;
-  }
+  PIDController(float Kp, float Ki, float Kd);
+  float calculateOutput(float setpoint, float measurement, float dt);
 
 private:
   float _Kp, _Ki, _Kd;
