5097c31d363111fa02f952d60b55e9eb9b751cec
Nour <noorha728@gmail.com>
2024-12-27 12:44:29 +0200

    adjusting pins

--- control/movement/Config.h
+++ control/movement/Config.h
@@ -2,10 +2,10 @@
 #define PINCONFIG_H
 
 #define DEADZONE 4
-#define OUTPUTLIMITS 65000.0
-#define RESOLUTION 200.0
+#define OUTPUTLIMITS 255.0
+#define RESOLUTION 44
 #define TIME_FREQ 30
-#define dt 0.1
+#define dt 0.03
 
 #define kp0 1
 #define kp1 1
@@ -16,17 +16,17 @@
 #define kd0 1
 #define kd1 1
 
-#define pin_A1 PB6
-#define pin_A2 PB7
+#define pin_A1 36
+#define pin_A2 34
 
-#define pin_B1 PA3
-#define pin_B2 PA2
+#define pin_B1 39
+#define pin_B2 35
 
-#define enable_pin_1 PB0
-#define enable_pin_2 PA7
-#define input1_1 PA6
-#define input1_2 PB7
-#define input2_1 PB13
-#define input2_2 PB12
+#define enable_pin_1 23
+#define enable_pin_2 22
+#define input1_1 21
+#define input1_2 19
+#define input2_1 18
+#define input2_2 5
 
 #endif
